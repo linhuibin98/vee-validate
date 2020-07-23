77fd78eea3ffc6cd0bca5e30b63180fb5abd27df
Abdelrahman Awad <logaretm1@gmail.com>
2020-07-23 04:40:05 +0200

    deploy

---
+++ _nuxt/static/1595472000/api/use-field/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1595472000",layout:"default",error:null,serverRendered:true,routePath:"\u002Fapi\u002Fuse-field",config:{appURL:"https:\u002F\u002Fvee-validate.logaretm.com\u002Fv4",algolia:{apiKey:"498c5d264196aacd7606fed9857deb44",indexName:"vee-validate"}}};
